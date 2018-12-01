05d31c8acc126a9fe01a62c890ba531808c01687
pawelbubak <pawelbubak97@gmail.com>
2018-12-01 17:58:21 +0100

    Usunięcie zbędnego pola

--- src/main/java/pl/put/poznan/sk/irc/controllers/T3Controller.java
+++ src/main/java/pl/put/poznan/sk/irc/controllers/T3Controller.java
@@ -3,25 +3,18 @@ package pl.put.poznan.sk.irc.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class T3Controller {
     @FXML
-    private TextField node_input;
-    @FXML
-    private VBox function_panel;
+    private VBox usersList;
 
     @FXML
     void initialize() {}
 
     //  ===============   INTERFACE FUNCTIONS   ===============
-    public void clear() {
-        node_input.setStyle("-fx-background-color: #a9a9a9 , white , white;");
-        node_input.setPromptText("");
-    }
 
     private void setPanel(String path) {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource(path));
@@ -30,7 +23,7 @@ public class T3Controller {
             root = loader.load();
         } catch (IOException ignored) {
         }
-        function_panel.getChildren().add(root);
+        usersList.getChildren().add(root);
     }
 
     //  ===============   OTHERS   ===============
